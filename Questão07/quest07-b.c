f5c67bb8a2a59536a1251967b21b47580f588517
rubens pinto <rubenssi825@gmail.com>
2020-09-19 19:02:28 -0300

    first commit

--- Questão07/quest07-b.c
+++ Questão07/quest07-b.c
@@ -3,8 +3,8 @@ int semente = 0;
 while ( semente <= 100; ) {
 
 	float sem1 = (float)semente *2;
-	sem2 = (float)semente + 1.5 * abc;
-	media = (float)(sem1 + sem2) /2;
+	float sem2 = (float)semente + 1.5 * abc;
+	float media = (float)(sem1 + sem2) /2;
 	printf("%f", media);
 	semente+=2;
 	
